8c223af1a8e6a844215bf652034343337119a650
Kennedy Sigauke <KSigauke@jhb.dvt.co.za>
2020-02-11 22:16:50 +0200

    Fix linting errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -50,7 +50,7 @@ describe('AppComponent', () => {
  * Although officially deprecated, some browsers (phantom) don't accept the preferred "new Event(eventName)"
  */
 export function newEvent(eventName: string, bubbles = false, cancelable = false) {
-  let evt = document.createEvent('CustomEvent');  // MUST be 'CustomEvent'
+  const evt = document.createEvent('CustomEvent');  // MUST be 'CustomEvent'
   evt.initCustomEvent(eventName, bubbles, cancelable, null);
   return evt;
 }
